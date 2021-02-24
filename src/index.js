47346cfdb71e0ce1749c844c29bfa5427ee269f8
Felix Swinkes <tgifelix@icloud.com>
2021-02-24 21:38:25 +0100

    style expirements

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { TerminalContextProvider } from "react-terminal";
 import App from './App';
+import "./style.css"
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
